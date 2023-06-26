6f64f15936f1a851fe3028e7d14ddb72a4c24c89
brianrono <brianrono16@gmail.com>
2023-06-26 22:40:05 +0300

    Modified routes

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -3,4 +3,13 @@ class PizzasController < ApplicationController
       pizzas = Pizza.all
       render json: pizzas, each_serializer: PizzaSerializer
   end
+
+  def show
+    pizza = Pizza.find_by(id: params[:id])
+    if pizza
+      render json: pizza, serializer: PizzaSerializer
+    else
+      render json: { error: 'Pizza not found' }, status: :not_found
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show, :destroy]
-  resources :pizzas, only: [:index, :show]
+  resources :pizzas, only: [:index, :show, :create]
   resources :restaurant_pizzas, only: [:create]
 end
